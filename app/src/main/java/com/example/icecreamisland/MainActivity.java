9c6fd7bb212783f41e350c218d5b7524a69d62f2
Waqar Ahmad <icodencode>
2021-09-02 18:46:52 +0530

    added Home Page

--- app/src/main/java/com/example/icecreamisland/MainActivity.java
+++ app/src/main/java/com/example/icecreamisland/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.icecreamisland;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button scoop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scoop = findViewById(R.id.scoop);
+
+        scoop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),HomePage.class);
+                startActivity(i);
+            }
+        });
+
     }
 }

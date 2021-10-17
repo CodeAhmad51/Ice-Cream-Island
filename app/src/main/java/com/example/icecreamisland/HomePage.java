734a7fb7a812d301f2f2db5a490d675a4a459fde
Istekhar Ahmad <codeahmad51@gmail.com>
2021-10-17 21:17:57 +0530

    added profile activity

--- app/src/main/java/com/example/icecreamisland/HomePage.java
+++ app/src/main/java/com/example/icecreamisland/HomePage.java
@@ -26,46 +26,124 @@ public class HomePage extends AppCompatActivity {
 
         qnt_3 = findViewById(R.id.qnt_3);
 
-        iceCream_price_remove_1 = findViewById(R.id.icecream_add_1);
-        iceCream_price_remove_2 = findViewById(R.id.icecream_add_2);
-        iceCream_price_remove_3 = findViewById(R.id.icecream_add_3);
+        iceCream_price_add_1 = findViewById(R.id.icecream_add_1);
+        iceCream_price_add_2 = findViewById(R.id.icecream_add_2);
+        iceCream_price_add_3 = findViewById(R.id.icecream_add_3);
 
-        iceCream_price_add_1 = findViewById(R.id.icecream_remove_1);
-        iceCream_price_add_2 = findViewById(R.id.icecream_remove_2);
-        iceCream_price_add_3 = findViewById(R.id.icecream_remove_3);
+        iceCream_price_remove_1 = findViewById(R.id.icecream_remove_1);
+        iceCream_price_remove_2 = findViewById(R.id.icecream_remove_2);
+        iceCream_price_remove_3 = findViewById(R.id.icecream_remove_3);
 
-       final int price1 , price2 , price3;
-        price1 = Integer.valueOf(iceCream_price_1.getText().toString());
-        price2 = Integer.valueOf(iceCream_price_2.getText().toString());
-        price3 = Integer.valueOf(iceCream_price_3.getText().toString());
+       final Float price1 , price2 , price3;
+        price1 = Float.valueOf(iceCream_price_1.getText().toString());
+        price2 = Float.valueOf(iceCream_price_2.getText().toString());
+        price3 = Float.valueOf(iceCream_price_3.getText().toString());
+
+        final int qnt1 , qnt2 , qnt3;
+
+        qnt1 = Integer.valueOf(qnt_1.getText().toString());
+        qnt2 = Integer.valueOf(qnt_2.getText().toString());
+        qnt3 = Integer.valueOf(qnt_3.getText().toString());
 
         iceCream_price_add_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int res;
+                Float res ;
+                int rslt;
 
-                res = Integer.valueOf(iceCream_price_1.getText().toString());
-                iceCream_price_1.setText(res + price1);
+                res = Float.valueOf(iceCream_price_1.getText().toString());
+                res = res + price1;
+                iceCream_price_1.setText(" " + res);
+
+
+                rslt = Integer.valueOf(qnt_1.getText().toString());
+                rslt +=1;
+                qnt_1.setText(""+rslt);
+
+            }
+        });
+
+        iceCream_price_remove_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Float res;
+
+                int  rslt;
+
+                res = Float.valueOf(iceCream_price_1.getText().toString());
+                rslt = Integer.valueOf(qnt_1.getText().toString());
+
+
+                if(price1<res){
+                    res = res - price1;
+                    iceCream_price_1.setText(" " + res);
+
+                    rslt= rslt - qnt1;
+                    qnt_1.setText(""+rslt);
+                }
+
+
             }
         });
 
         iceCream_price_add_2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int res;
+                Float res;
+                int  rslt;
 
-                res = Integer.valueOf(iceCream_price_1.getText().toString());
-                iceCream_price_1.setText(res + price2);
+                res = Float.valueOf(iceCream_price_2.getText().toString());
+                res = res + price2;
+                iceCream_price_2.setText(" "+ res );
+
+                rslt = Integer.valueOf(qnt_2.getText().toString());
+                rslt+=1;
+                qnt_2.setText(""+rslt);
+
+            }
+        });
+
+        iceCream_price_remove_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Float res;
+                int  rslt;
+
+
+                rslt = Integer.valueOf(qnt_2.getText().toString());
+                res = Float.valueOf(iceCream_price_2.getText().toString());
+
+                if(price2<res){
+                    res = res - price2;
+                    iceCream_price_2.setText(" " + res);
+
+                    rslt= rslt - qnt2;
+                    qnt_2.setText(""+rslt);
+                }
+
             }
         });
 
         iceCream_price_add_3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int res;
+                Float res;
 
-                res = Integer.valueOf(iceCream_price_1.getText().toString());
-                iceCream_price_1.setText(res + price3);
+                res = Float.valueOf(iceCream_price_3.getText().toString());
+                res = res + price3;
+                iceCream_price_3.setText(" "+res);
+
+            }
+        });
+
+        iceCream_price_remove_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Float res;
+
+                res = Float.valueOf(iceCream_price_3.getText().toString());
+                res = res - price3;
+                iceCream_price_3.setText(" " + res);
 
             }
         });
---
+++ app/src/main/java/com/example/icecreamisland/ProfilePage.java
@@ -0,0 +1,51 @@
+package com.example.icecreamisland;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class ProfilePage extends AppCompatActivity {
+
+    EditText username_profilePage, password_ProfilePage , email_profile;
+
+    SharedPreferences sharedPreferences_profile;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_page);
+        username_profilePage = findViewById(R.id.username_profilePage);
+        password_ProfilePage = findViewById(R.id.password_profilePage);
+        email_profile = findViewById(R.id.Email_editText);
+
+
+        sharedPreferences_profile = getSharedPreferences("username_dataBase",MODE_PRIVATE);
+
+        username_profilePage.setText(getUsername("username_key"));
+        password_ProfilePage.setText(getPassword("password_key"));
+        email_profile.setText(getEmail("email_key"));
+
+
+    }
+
+
+    String getUsername(String key){
+        String s;
+        s = sharedPreferences_profile.getString(key," ");
+        return s ;
+    }
+    String getPassword(String key){
+        String password;
+        password = sharedPreferences_profile.getString(key," ");
+        return password ;
+    }
+    String getEmail(String key){
+        String email;
+        email=sharedPreferences_profile.getString(key," ");
+        return email;
+    }
+
+}
